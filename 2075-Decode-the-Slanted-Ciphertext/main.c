092469cf1ef925ee2487e0e35326ba2bd182744d
Viktor Vyhnálek <vyhnavik@cvut.cz>
2025-11-25 22:26:21 +0100

    2075: works for like 5 tests

--- 2075-Decode-the-Slanted-Ciphertext/main.c
+++ 2075-Decode-the-Slanted-Ciphertext/main.c
@@ -1,39 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
-// works but not for matrix
-void decodeCiphertext(char* encodedText, int rows, char** decodedText) {
-    int rowLength;
-    if (rows>1) {
-        rowLength = ((int)strlen(encodedText) - rows+1) / rows;
-    }
+char* decodeCiphertext(char* encodedText, int rows) {
+    int strLen = (int) strlen(encodedText)-(rows-1)*2;
+    int cols, cp=0;
+    if (rows==1) {cols = strLen;}
     else {
-        rowLength = ((int)strlen(encodedText));
+        cp = strLen-rows+1;
+        if (cp%rows == 0) {cols = cp/rows;}
+        else {cols = cp/rows+1;}
     }
-
-    char* originalText= (char*) malloc(sizeof(char)*rowLength*rows+1);
-    for (int i=0; i<rowLength; i++) {
-        for (int j=0; j<rows; j++) {
-            originalText[i*rowLength+j] = encodedText[j*rowLength + i + j];
+    char* originalText = (char*) malloc(sizeof(char)*(cols*rows)+1);
+    int Idx=0;
+    int spaces = 2;
+    for (int c=0; c<cols; c++) {
+        for (int r=0; r<rows; r++) {
+            int pos = (cp%rows!=0) ? c+r*(rows+1+spaces) : c+r*(rows+spaces);
+            originalText[Idx] = encodedText[pos];
+            Idx++;
         }
     }
-    originalText[rowLength * rows] = '\0';
-
-    *decodedText = strncpy(originalText, encodedText, rowLength*rows);
-    (*decodedText)[rowLength*rows]= '\0';
+    originalText[Idx]= '\0';
+    printf("%s\n", originalText);
+    return originalText;
 }
 
-constexpr int MAX_LEN = 101;
-int main() {
-    int rows;
-    printf("enter num of rows:\n");
-    if (scanf("%d", &rows) != 1 || rows <= 1 || rows > 1000) {return 1;}
-    char* str = (char*) malloc(sizeof(char)*MAX_LEN);
-    printf("enter string:\n");
-    char* decodedText=NULL;
-    decodeCiphertext(str, rows, &decodedText);
-    printf("%s\n",decodedText);
-    free(decodedText);
+int main(void) {
+    assert(strncmp(decodeCiphertext("ch   ie   pr", 3),"cipher", 7)==0);
+    assert(strncmp(decodeCiphertext("coding", 1), "coding", 7)==0);
+    assert(strncmp(decodeCiphertext("iveo    eed   l te   olc", 4),"i love leetcode", 16)==0);
     return 0;
 }
